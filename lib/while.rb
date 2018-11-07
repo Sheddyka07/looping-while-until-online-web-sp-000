83bccdb3bcdd10702477e4b31670b6e5cb8844eb
Kelly Sheddy <jungaka07@gmail.com>
2018-11-07 23:24:19 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,9 @@
 def using_while
-  counter = 0
-  while counter < 6
+  levitation_force = 6
+  while levitation_force < 10
   loop do 
     puts "Wingardium Leviosa"
-    counter += 1
+    levitation_force += 1
 	end 
 end
 
